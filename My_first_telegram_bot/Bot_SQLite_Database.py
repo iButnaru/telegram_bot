b9a466267325644ba0198c6492464a50be003ab1
Ionut Butnaru <ionut.butnaru95@yahoo.com>
2018-08-22 11:22:44 +0300

    added new functions - add_items -  add new elements to the Database, delete_items - delete elements from the Database, get_item - gets a list with all the elements from the Database.

--- My_first_telegram_bot/Bot_SQLite_Database.py
+++ My_first_telegram_bot/Bot_SQLite_Database.py
@@ -9,3 +9,21 @@ class DBStore:
     def create_table(self):
         self.conn.execute("CREATE TABLE IF NOT EXISTS items (description text)")
         self.conn.commit()
+
+    def add_items(self, item):
+        args = item
+        self.conn.execute("INSERT INTO items (description) VALUES (?)",
+        (args,))
+        self.conn.commit()
+
+    def delete_items(self, item):
+        args = item
+        self.conn.execute("DELETE FROM items WHERE description = (?)",
+                          (args,))
+        self.conn.commit()
+
+    def get_item(self):
+        stmt = "SELECT description FROM items"
+        return [x[0] for x in self.conn.execute(stmt)]
+
+
